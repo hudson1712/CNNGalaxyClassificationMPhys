f1504575a082330e57bf02c21b1d78526eec4674
hudson1712 <99171986+hudson1712@users.noreply.github.com>
2022-10-25 12:08:07 +0100

    Update calibration_plot.py

--- calibration_plot.py
+++ calibration_plot.py
@@ -33,18 +33,21 @@ def main():
         print("CUDA not available")
 
 #------------------------------------------------------------------------------
-#   Parameters
+#Parameters
 
-    M = 5 #number of bins for historgram
-    csv_file = pd.read_csv('CN4_rotations/overlap_NO_ROT.csv')
-    
+    model_to_plot = 'CN8'
+    M = 13 #number of bins for histogram
+    csv_file = pd.read_csv(model_to_plot + '_rotations/overlap.csv')
     
 #------------------------------------------------------------------------------
-    
-    bins = np.linspace(0,1,M+1,endpoint=True)
+#Define variables
+
+    bins = np.zeros(M)
+    bin_centres = np.zeros(M)
     accuracy = np.zeros(M)
     confidence = np.zeros(M)
     uncertainty = np.zeros(M)
+    error = np.zeros(M)
     ECE=0
     UCE=0
     
@@ -54,6 +57,13 @@ def main():
     softmax_probs_str = csv_file['softmax prob'].values
     softmax_probs = np.empty([len(softmax_probs_str)])
     overlaps = csv_file['average overlap'].values
+    dataset_size = len(target)
+    bin_width = int(dataset_size / M)
+    # if(dataset_size % M != 0):
+    #     bin_width += 1
+    
+    #print(bin_width)
+    #print(overlaps)
     
     i = 0
     for str in softmax_probs_str:
@@ -61,22 +71,46 @@ def main():
         softmax_probs[i] = (1 - float(temp.split()[0]))
         i += 1
     
+    softmax_probs_sorted = softmax_probs[softmax_probs.argsort()]
+    target = target[softmax_probs.argsort()]
+    overlaps = overlaps[softmax_probs.argsort()]
+    
+    #print(softmax_probs_sorted)
+    #print(overlaps)
+    
 #Bin data
     
-    binned_data = np.digitize(softmax_probs, bins)
+    i=0
+    for i in range(M-1):
+        bins[i] = np.quantile(softmax_probs_sorted, (i+1)/M)
+        prev_value = 0
+        if i != 0:
+            prev_value = bins[i-1]
+        bin_centres[i] = (bins[i] - prev_value)/2 + prev_value
+    bins[M-1] = 1
+    bin_centres[M-1] = (1 - bins[M-2])/2 + bins[M-2] 
+    
+    print(bin_centres)
     
     classifications = np.zeros(M)
     bin_sizes = np.zeros(M)
     
+#Calculate the total confidence, uncertainty and number of points in each bin
+
     i=0
-    for b in binned_data:
-        confidence[b-1] += softmax_probs[i] #running total of confidence
-        uncertainty[b-1] += overlaps[i] #running total of uncertainty
-        bin_sizes[b-1] += 1 #total in bin
-        if target[i] == 1:
-            classifications[b-1] += 1 #total classified in bin
-        i += 1
+    for b in range(M):
+        for j in range(bin_width):
+            print(j + (b)*bin_width)
+            confidence[b-1] += softmax_probs[j + (b)*bin_width] #running total of confidence
+            uncertainty[b-1] += overlaps[j + (b)*bin_width] #running total of uncertainty
+            bin_sizes[b-1] += 1 #total in bin
+            if target[j + (b-1)*bin_width] == 1:
+                classifications[b-1] += 1 #total classified in bin
+            i += 1
+        
     total_datapoints = i
+    
+    print('The bin sizes are ', bin_sizes)
 
 #Calculate average accuracy,uncertainty,confidence and ECE and UCE
 
@@ -85,25 +119,25 @@ def main():
         accuracy[i] = classifications[i]/bin_sizes[i]
         uncertainty[i] /= bin_sizes[i]
         confidence[i] /= bin_sizes[i]
-        error[i] = 1 - accuracy[i]
+        error[i] = (1 - accuracy[i])
         ECE += (bin_sizes[i]/total_datapoints)*abs(accuracy[i] - confidence[i])
-        i+=1
         UCE += (bin_sizes[i]/total_datapoints)*abs(error[i] - uncertainty[i])
-    print(uncertainty)
+        i+=1
         
 #Format ECE and UCE, then plot graphs
 
     ECE = np.format_float_positional(ECE, precision=4)
+    UCE = np.format_float_positional(UCE, precision=4)
     
     bins = bins + 1/(2*M) #shift each bin so that it is central
     plt.subplot(111)
-    plt.plot(bins[:M], accuracy[:M])
+    plt.scatter(bin_centres[:M], accuracy[:M])
     
     x = np.linspace(0,1,10)
     y =np.linspace(0,1,10)
     plt.plot(x,y)
     plt.grid
-    plt.title("Model Calibration Plot: CN4 Test data")
+    plt.title("Model Calibration Plot: " + model_to_plot + " Test data")
     plt.xlabel("Confidence")
     plt.ylabel("Accuracy")
     textstr = ("ECE =  " + ECE)
@@ -112,6 +146,21 @@ def main():
         verticalalignment='top', bbox=props)
     plt.show()
     
+    plt.subplot(111)
+    plt.scatter(uncertainty[:M], error[:M])
+    
+    x = np.linspace(0,1,10)
+    y =np.linspace(0,1,10)
+    plt.plot(x,y)
+    plt.grid
+    plt.title("Model Calibration Plot: " + model_to_plot + " Test data")
+    plt.xlabel("Uncertainty")
+    plt.ylabel("Error")
+    textstr = ("UCE =  " + UCE)
+    props = dict(boxstyle='square', facecolor='silver', alpha=0.5)
+    plt.text(0.05, 0.95, textstr, fontsize=14,
+        verticalalignment='top', bbox=props)
+    plt.show()
     
     return
 
